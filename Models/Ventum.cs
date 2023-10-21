8c4d8b0a068d09092220188c317d7af2ab151695
ClaudioVergara2 <claudio.vergara2@virginiogomez.cl>
2023-10-21 18:53:58 -0300

    [style] Venta (lista de ventas)

--- Models/Ventum.cs
+++ Models/Ventum.cs
@@ -1,25 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace API_Ventas.Models;
 
 public partial class Ventum
 {
+    [JsonIgnore]
     public int IdVenta { get; set; }
 
-    public int IdProducto { get; set; }
-
     public string NomUsuario { get; set; } = null!;
+
+    public int IdProducto { get; set; }
 
     public int Cantidad { get; set; }
 
     public int Total { get; set; }
-
+    [JsonIgnore]
     public DateTime FechaVenta { get; set; }
 
     public int Estado { get; set; }
-
+    [JsonIgnore]
     public virtual Producto IdProductoNavigation { get; set; } = null!;
-
+    [JsonIgnore]
     public virtual Usuario NomUsuarioNavigation { get; set; } = null!;
 }
